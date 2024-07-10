882a070eb30e93de970f7629fe5b8d1c6ff88512
Bartosz55245 <dsw55245@student.dsw.edu.pl>
2024-07-10 22:31:22 +0200

    Task2

--- Konwerter.py
+++ Konwerter.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import json
 
 def parsowanie():
     if len(sys.argv) != 3:
-        print("Jak używać: program.exe plik1.x plik2.y")
+        print("Jak używać: program.exe pathFile1.x patchFile2.y")
         sys.exit(1)
 
     input_file = sys.argv[1]
@@ -18,5 +19,19 @@ def parsowanie():
 
     return input_file, output_file, input_format, output_format
 
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Nie znaleziono pliku: {file_path}")
+        sys.exit(1)
+
 if __name__ == "__main__":
     input_file, output_file, input_format, output_format = parsowanie()
+    if input_format == '.json':
+        data = read_json(input_file)
